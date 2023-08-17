59d74369424e2bf163954214ad56a921522a9ec4
Gbenga Aiyejumo <41402706+gaiyejumo@users.noreply.github.com>
2023-08-17 08:52:30 +0100

    Create main.py
    My first line of code, detailing the few things I need to consider in my code.

---
+++ main.py
@@ -0,0 +1,3 @@
+# We will attempt to create a code that can achieve this program.
+# First we need the Twitter automate code and how it functions.
+# Then I will proceed to getting the API reponsible for providing live market rates.
